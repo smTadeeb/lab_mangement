8e4d87093c47da6f96aa0038150d3892988c55a1
Sheikh Muhammed Tadeeb <70131688+smTadeeb@users.noreply.github.com>
2020-09-26 21:08:08 +0530

    Update Lab Management.cpp

--- Lab Management.cpp
+++ Lab Management.cpp
@@ -1,25 +1,24 @@
-#include<iostream>                                                   
-#include<map>
-#include<string>                                                        
-#include<windows.h> 
-#include<conio.h>
-#include <stdexcept> 
-#include<fstream>                                         
+#include<iostream>                                                   //input output system.
+#include<map>                                                       //dictionary mapping used.                                                      
+#include<windows.h>                                                //graphic integration and attributes function. 
+#include<conio.h>                                                 //console opening.
+#include <stdexcept>                                             //exceptional handling.
+#include<fstream>                                               //file handler.
 using namespace std;                                           
 
-class helpline_team
+class helpline_team                                            //master class which is used to deliver data to sub classes.
 {	
-    public:                                                        
-    helpline_team(){}                                                          
+    public:                                                     //access specifier.
+    helpline_team(){}                                           //constructor and destructor.               
 	~helpline_team(){} 	                                                        
     private:	                                               
-    void check()                                            
+    void check()                                                //private method used for login access.
     	{                                                   
     	int svr;                                           
-    	string userName,userPassword="";
+    	string userName,userPassword="";                       //password masking using char and string.
     	char pw =' ';
         int loginAttempt = 0;                           
-        while (loginAttempt < 2)                         
+        while (loginAttempt < 2)                              //looping to start again if incorrect password or username entered with limit. 
         {                                               
         cout<<"\n Please enter your user name: ";
         cin>>userName;
@@ -30,12 +29,12 @@ class helpline_team
 		if(pw!=13)
 		{
 		userPassword +=pw;
-		cout<<"*";
+		cout<<"*";                                                //masking or encryption of original password into a symbol. 
         }
         }
         if ((userName == "Tadeeb" || userName == "Nipun") && (userPassword == "T@covid19" || userPassword == "N@covid19"))                  
         {                                                                       
-		cout<<"\n\n\n\t\t\t\t\t\t Welcome "<<userName<<" "<<"!\n";                          
+		cout<<"\n\n\n\t\t\t\t\t\t Welcome "<<userName<<" "<<"!\n";                          //username and password.
 		break;                                                                  
         }                                                                    
         else
@@ -59,8 +58,8 @@ class helpline_team
  (1 for low)\n\n\
  Enter the number to know the details = ";                                         
 	    cin>>svr;                                                                             
-        if (svr==1)                                                                          
-        {                                                                                  
+        if (svr==1)                                                           // chencking the testing level of lab with if-else conditions.               
+        {                                                                     // looping back using while if invalid input given.             
         cout<<"\n\n This lab check patients with low intensity symptoms.\n\
  Lab Name: Ayur Tests.\n\
  Location: Freeganj Ujjain (M.P).\n\
@@ -98,21 +97,21 @@ class helpline_team
 		}                                                            
 	    void data_frm_ht()                                          
 		{                                                          
-	 	 A_total_patients = 80;                                   
+	 	 A_total_patients = 80;                                   //displaying no. of patients attended in respective labs.
 		 B_total_patients = 40;                            
 		 C_total_patients = 60;    
-		 total = A_total_patients + B_total_patients + C_total_patients;                            
+		 total = A_total_patients + B_total_patients + C_total_patients;    //total expected patients to come in a day.                           
 	    }                                                      
 };                                                             
-class patient_info : protected helpline_team               
+class patient_info : protected helpline_team               //Sub-Class patient lab which extracts data from base class.
 {
 	public:
 		// Destructor for clearing memory space.
 		~patient_info(){}
 	protected:                                                                                 
-	    void display_info()                                                              
+	    void display_info()                            //method which calls base class protected data and further operations.                                                              
 	    { 
-		cout<<"\n\n ______________________________________________________Log-in__________________________________________________________ \n\n";                                                                               
+		cout<<"\n\n ____________________________________________Log-in___________________________________________________ \n\n";                                                                               
 	    call();                                                                          
 		data_frm_ht();
 		string input;
@@ -121,7 +120,7 @@ class patient_info : protected helpline_team
 		{
 		cout<<"\n\n\n Do you want to know the nos of patients coming to different labs(y/n) :";
 		cin>>input;
-		if ((input == "y") || (input == "Y"))
+		if ((input == "y") || (input == "Y"))                       //ask user wheather to see no. of patients data. 
 		{
 		cout<<"\n Total number of patients coming to the different labs are\n\
  \n lab Ayur Tests:- "<<A_total_patients;
@@ -144,24 +143,24 @@ class patient_info : protected helpline_team
 		call_1();
 	    }
     // Abstraction & Encapsulation                                       
-	private:                                                                                    
-		long long int age,ph_num,h_num;                                   
+	private:                                                                                                                  
+		long long int age,ph_num;                                   
 		float DNA_enzyme,RNA_enzyme,nucleotide;                     
-		void registration()                                          
+		void registration()                            // method which create a database of user registration.               
 		{    
 		 ofstream dbms;
-		 dbms.open("database.txt",ios_base::app);
+		 dbms.open("database.txt",ios_base::app);           //flie creation named database and no overriting.
 		    char f_name[100],name[100],time[100],city[100],address[100];                                           
-			cout<<"\n\n\n ____________________________________________ Patient registration forum _____________________________________________"; 
+			cout<<"\n\n\n _____________________________________ Patient registration forum ____________________________________"; 
 			cout<<"\n\n\n Enter your name: ";                      
 		    cin.ignore(); 
 			cin.getline(name,100);                                  
 			dbms<<name<<endl;
-			cout<<"\n Enter your fathers name: ";                
+			cout<<"\n Enter your fathers name: ";           //patients details such as name,no, city, age etc.     
 		    cin.ignore(); 
 			cin.getline(f_name,100);
 			dbms<<f_name<<endl;
-			cout<<"\n Enter your city & state name: ";      //cin.ignore() before and after and cin.clear()  Ask mam.      
+			cout<<"\n Enter your city & state name: ";      
 			cin.ignore();
 			cin.getline(city,100);                                                                 
 			dbms<<city<<endl;
@@ -173,14 +172,16 @@ class patient_info : protected helpline_team
 			cin.ignore();
 			cin.getline(address,100);                                                            
 			dbms<<address<<endl;
+			int g;
+			//Exceptional handling for age.
 			try
 			{
-			cout<<"\n Enter your age: ";                     
+			cout<<"\n Enter your age: ";           // if user types string in age the error will get handled by try-catch and ask user to retype.
 			cin>>age;
 			dbms<<" "<<age<<endl;                                  
-			if (cin.fail())      {throw runtime_error("\n Input is not an integer\n");}                                                     
-		    }
-			catch(const runtime_error& e)
+			if (!(age>0 && age<=1000))     
+			{throw 5;}                                                     
+			}catch(int l)
 		    {
 		    	cerr<<"\n Invalid Input. Please enter valid input. Its runtime_error ";
 			}
@@ -191,13 +192,13 @@ class patient_info : protected helpline_team
 			cout<<"\n\n Test: For Covid-Sars-19, Kindly submit Rs 2500/- to the counter desk";
 		}
 			     
-		void test()                                                                    
+		void test()                      //method to calculate whether a person is infected with covid or not.                                                                    
 		{                                                                                
 			float fluroscense = 15.6;                                                  
-			cout<<"\n\n\n\n ______________________________________________Lab, Test in progress ________________________________________________";                        
+			cout<<"\n\n\n\n _______________________________________Lab, Test in progress _________________________________________";                        
 			cout<<"\n\n\n\n Enter patient DNA enzyme %: ";                         
 			cin>>DNA_enzyme;                                                         
-			cout<<"\n Enter patient RNA enzyme %: ";                            
+			cout<<"\n Enter patient RNA enzyme %: ";                            //formula is made by us only.
 			cin>>RNA_enzyme;
 			cout<<"\n Enter patient Nucleotide level: ";
 			cin>>nucleotide;
@@ -218,9 +219,9 @@ class patient_info : protected helpline_team
 				cout<<"\n\n Patient is highly impacted with COVID-19";                   
 		        	string a,b;                                                     
 		        	a = "City Lab";                                                    
-	                b = "Sanjeevni Lab";                                            
-					string *x=&a,*y=&b,t;                                         
-					t = *x;                                                        
+	                b = "Sanjeevni Lab";                   //use of pointer to swap the patient who is highly infected with corona.
+					string *x=&a,*y=&b,t;                 //in the high intensity dealing patients lab. 
+					t = *x;                              //if the sample exceeds a particular amount of nucleotide level.
 					*x = *y;                                                       
 					*y = t;                                                       
 					cout<<"\n\n Our lab treats moderate patients, You can refer to: "<<b; 		
@@ -228,44 +229,44 @@ class patient_info : protected helpline_team
 			}
 		}                                                                      
 	protected:                                                        
-	    void timeSlots()                                                     
+	    void timeSlots()               // filtering time into time slots for providing results to batches of divided time.
 		{                                                                    
 			string morning[4] = {"9-10AM","10-11AM","11-12noon","12-1PM"};  
 			string evening[4] = {"2-3PM","3-4PM","4-5PM","5-6PM"};	      
 		}	                                                           
-		void call_1()                                                    
+		void call_1()           // method to call private methods of this class.                                         
 		{                                                              
 			registration();                                            
 			test();                                                  
 		}
 };                                            
-class lab_info : public patient_info        
+class lab_info : public patient_info        //3rd sub class use to inherit the data from above classes.
 {                                          
 	public: 
-	    void show()
+	    void show()                //method to call operator function.
 		{
 			operator --();
     	}                             
-		void display_info()               
+		void display_info()               //method to call private method of this class only.
 		{                                
 			chemical();                
 		}                           
 	~lab_info(){}                           
 	protected:                      
-		void chemical()           
+		void chemical()           //method to check the list of available chemicals present inside the lab with quantity.
 		{		                  
 			string i;
-			std::map< string , string > chem_list;                                    
+			std::map< string , string > chem_list;         //using of map to create dictionary.                           
 			chem_list.insert(pair<string , string>("a","Acetaminophen (500 mL)"));  
 			chem_list.insert(pair<string , string>("h","Haloperidol (300 mL)"));    
 			chem_list.insert(pair<string , string>("e","Esomeprazole (100 mL)"));  
-			cout<<"\n\n\n ______________________________________________ Chemicals in lab _____________________________________________________";                          
+			cout<<"\n\n\n ______________________________________ Chemicals in lab ____________________________________________";                          
 			cout<<"\n\n Enter an alphabet(a,h,e):-";                            
 			cin>>i;                                                            
-			if (i == "a")                                                      
+			if (i == "a")                       //taking input from alphabet to check list in order.                               
 			{                                                                
-			   cout<<"\n "<<chem_list["a"];                                 
-			}                                                               
+			   cout<<"\n "<<chem_list["a"];         //calling of the array of list from alphabets typed. 
+			}                                                              
 			if (i == "h")                                                   
 			{                                                             
 			   cout<<"\n "<<chem_list["h"];                              
@@ -275,23 +276,23 @@ class lab_info : public patient_info
 			   cout<<"\n "<<chem_list["e"];                           
 			}                                                       
 	        }
-	    void operator -(int un_reg)  
+	    void operator -(int un_reg)                                 //operator function used to calculate total patients tested.
 	    { cout<<"\n\n Patient data from helpline team: "<<total;
 	      total = total - un_reg;
 	      cout<<"\n Today's unregistered patients: "<<un_reg;
 	      cout<<"\n Data sent to Govt. :"<<total;
 		}
 	private:
-	void operator --()
+	void operator --()               //operator function and method overloading to call all methods inherited from different classes.
 	{
-		//int un_reg = 30;
+		
 		lab_info obj1;
 		obj1.patient_info::display_info();
 		obj1.operator -(30);
 		display_info();
 		string input2;
 		int attempt2;
-		cout<<"\n\n\n ____________________________________________ Weekly Report of lab ____________________________________________________"; 
+		cout<<"\n\n\n ___________________________________ Weekly Report of lab _____________________________"; 
 		while(attempt2<5)
 		{
 		cout<<"\n\n\n Do you want to check weekly report(y/n) : ";
@@ -300,7 +301,7 @@ class lab_info : public patient_info
 		{
 		string data;
 		ifstream repo;
-		repo.open("report.txt");
+		repo.open("report.txt");      //asking user wheather to see the weekly report from filehandling created earlier.
 		while(getline(repo,data))
 		{
 		cout<<"\n"<<data;
@@ -324,11 +325,14 @@ class lab_info : public patient_info
 };
 int main()                                    
 {     
-    system("Color C0");                                                   
-	lab_info obj1; 
-	obj1.show();                                              
+    system("Color C0");               // changing the foreground and background color.
+	lab_info obj1;                   //creating the object of the sub class 3.
+	obj1.show();                    //calling the method containing data inherited using multiple inheritence.                          
 	return 0;                           
-}                                        
+}                                      
+
+
+       //..................................THANK YOU......................................................................//  
        
     
         
